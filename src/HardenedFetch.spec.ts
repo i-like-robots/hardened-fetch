94533681c6797cee49a687143b885c88c769bb4a
Matt Hinchliffe <matt@maketea.co.uk>
2023-12-08 20:37:46 +0000

    Remove redundant tests from top level test suite

--- src/HardenedFetch.spec.ts
+++ src/HardenedFetch.spec.ts
@@ -57,40 +57,11 @@ describe('Hardened Fetch', () => {
       assert.ok(response instanceof Response)
     })
 
-    it('resolves with the Response object', async () => {
-      mockClient.intercept({ path: '/' }).reply(200, 'OK')
-
-      const instance = new HardenedFetch()
-      const response = await instance.fetch('http://www.example.com/')
-
-      assert.ok(response instanceof Response)
-    })
-
-    it('provides a rate limiter to the request function', async () => {
-      const date = new Date()
-      const wait = 1
-
-      mockClient.intercept({ path: '/' }).reply(429, 'Rate Limit Exceeded', {
-        headers: {
-          'X-RateLimit-Reset': String(wait),
-          Date: date.toISOString(),
-        },
-      })
-
-      mockClient.intercept({ path: '/' }).reply(200, 'OK')
-
-      const instance = new HardenedFetch()
-      const response = await instance.fetch('http://www.example.com/')
-
-      assert.ok(response.ok)
-      assert.ok(Date.now() - date.getTime() >= wait * 1000)
-    })
-
     it('throws an error on failure', async () => {
       mockClient.intercept({ path: '/' }).reply(500)
 
       const instance = new HardenedFetch({ retries: 0 })
-      assert.rejects(() => instance.fetch('http://www.example.com/'), Error)
+      assert.rejects(() => instance.fetch('http://www.example.com/'), 'InternalServerError')
     })
   })
 
